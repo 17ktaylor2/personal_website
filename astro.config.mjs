30b301e63f1b766ba599f84fe8139c8ddc9dae84
kaden <17ktaylor@gmail.com>
2026-07-01 20:50:16 -0600

    Add github deploy stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://17ktaylor2.github.io',
+	base: '/personal_website/',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
